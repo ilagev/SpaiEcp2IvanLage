7595277433b5f83f22f0c6e12fdb4d609b30dfd0
Fernando Moro <fernando.moro@radmas.com>
2015-10-04 21:27:31 +0200

    Updated FractionTest to test isLowerThan()

--- src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
@@ -7,28 +7,42 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class FractionTest {
-    
+
     private Fraction fraction;
-    
+
     @Before
     public void prepare() {
         fraction = new Fraction(3, 2);
     }
-    
+
     @Test
     public void testData() {
         assertEquals(2, fraction.getDenominator());
         assertEquals(3, fraction.getNumerator());
     }
-    
+
     @Test
     public void testDecimal() {
         assertEquals(1.5, fraction.decimal(), 10e-5);
     }
-    
-     @Test
+
+    @Test
     public void testEquivalent() {
-        assertTrue(fraction.isEquivalent(new Fraction(6,4)));
+        assertEquals(true, fraction.isEquivalent(new Fraction(6, 4)));
     }
 
+    @Test
+    public void testIsLowerThanWithLowerFraction() {
+        assertEquals(false, fraction.isLowerThan(new Fraction(16, 23)));
+    }
+
+    @Test
+    public void testIsLowerThanWithHigherFraction() {
+        assertEquals(true, fraction.isLowerThan(new Fraction(16, 3)));
+    }
+
+    @Test
+    public void testIsLowerThanWithSameFraction() {
+        assertEquals(false, fraction.isLowerThan(fraction));
+    }
 }
